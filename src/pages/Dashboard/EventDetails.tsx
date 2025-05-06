f82395aa02cb041f5f2d044c7bac1dc3a482f402
YingjingLu <yingjinglu2019us@gmail.com>
2025-05-05 22:51:32 -0700

    Handle edge case

--- src/pages/Dashboard/EventDetails.tsx
+++ src/pages/Dashboard/EventDetails.tsx
@@ -3,14 +3,15 @@ import DefaultLayout from "../../layout/DefaultLayout";
 import { useState, useEffect } from "react";
 import { PastEventSymbolPerformance } from "../../models/economic_events";
 import { BACKEND_URL } from "../../gobal/constants";
-import SinpleLineChart from "../../newComponents/SinpleLineChart";
+import SinpleLineChart, { SinpleLineChartProps } from "../../newComponents/SinpleLineChart";
 import EconomicEventStat from "../../newComponents/economicEventStat";
-
+import Spinner from "../../newComponents/Spinner";
 const EventDetails = () => {
 
     const { event } = useParams();
     console.log(event);
     const [eventSymbolPerformance, setEventSymbolPerformance] = useState<PastEventSymbolPerformance>(null);
+    const [loadingPastEventSymbolPerformance, setLoadingPastEventSymbolPerformance] = useState(true);
 
     useEffect(() => {
         const fetchEventSymbolPerformance = async () => {
@@ -18,6 +19,7 @@ const EventDetails = () => {
             const data = await response.json();
             const pastEventSymbolPerformance = data as PastEventSymbolPerformance;
             setEventSymbolPerformance(pastEventSymbolPerformance);
+            setLoadingPastEventSymbolPerformance(false);
         };
         fetchEventSymbolPerformance();
     }, [event]);
@@ -27,41 +29,81 @@ const EventDetails = () => {
         return date.substring(0,10);
       }
 
-    const createHistoricalPerformance = (performance: PastEventSymbolPerformance | undefined): SinpleLineChartProps => {
+      const shouldRenderPastSymbolPerformance = (performance: PastEventSymbolPerformance | undefined) => {
+        if (performance === undefined || !performance) {
+          return false;
+        }
+        if (performance.event_history.length < 3) {
+          return false;
+        }
+        if (performance.event_history[0].actual == null
+          && performance.event_history[1].actual == null
+          && performance.event_history[2].actual == null 
+          && performance.performance.in_1_day.length == performance.performance.in_1_hour.length) {
+          return true;
+        }
+        return false;
+      }
+
+      const createHistoricalPerformance = (performance: PastEventSymbolPerformance | undefined): SinpleLineChartProps => {
         if (performance === undefined || !performance) {
           return {
             actual: [],
             expected: [],
             x_axis: [],
+            actual_col_name: 'Actual',
+            expected_col_name: 'Expected',
           };
         }
         const actual: number[] = [];
         const expected: number[] = [];
         const x_axis: string[] = [];
-        for (const retrieved_event of performance.event_history) {
-          if (retrieved_event.actual !== undefined && retrieved_event.estimate !== undefined && retrieved_event.event_date !== undefined) {
-            actual.push(retrieved_event.actual);
-            expected.push(retrieved_event.estimate);
-            x_axis.push(`${formatDateToMMDD(retrieved_event.event_date)}`);
+        if (shouldRenderPastSymbolPerformance(performance)) {
+          for (let index = 0; index < performance.event_history.length; index++) {
+            const event = performance.event_history[index];
+            if (event.actual !== undefined && event.event_date !== undefined) {
+              actual.push(parseFloat(performance.performance.in_1_hour[index].toFixed(2)));
+              if (event.estimate !== null) {
+                expected.push(parseFloat(event.estimate.toFixed(2)));
+              }
+              x_axis.push(`${formatDateToMMDD(event.event_date)}`);
+            }
           }
+          return {
+            actual,
+            expected,
+            x_axis,
+            actual_col_name: '1 Hour SPY Change',
+            expected_col_name: '1 Day SPY Change',
+          };
+        } else {
+          for (const event of performance.event_history) {
+            if (event.actual !== undefined && event.event_date !== undefined) {
+              actual.push(event.actual);
+              expected.push(event.estimate);
+              x_axis.push(`${formatDateToMMDD(event.event_date)}`);
+            }
+          }
+          return {
+            actual,
+            expected,
+            x_axis,
+            actual_col_name: 'Actual',
+            expected_col_name: 'Expected',
+          };
         }
-        return {
-          actual,
-          expected,
-          x_axis,
-        };
       }
 
-    const renderEconomicEventStat = (performance: PastEventSymbolPerformance | undefined) => {
-        if (performance === undefined || !performance) {
-          return <div>Loading...</div>;
+      const renderEconomicEventStat = (performance: PastEventSymbolPerformance | undefined) => {
+        if (loadingPastEventSymbolPerformance) {
+          return <Spinner />;
         }
         if (performance.error) {
           return (
           <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
             <div className="flex flex-col gap-7.5 p-4 sm:p-6 xl:p-9">
               <h3 className="mb-5 text-title-md2 font-bold text-black dark:text-white">
-                The specified event was not found!
+                No important events in US today!
               </h3>
             </div>
           </div>);
@@ -72,13 +114,20 @@ const EventDetails = () => {
             <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
               <div className="flex flex-col gap-7.5 p-4 sm:p-6 xl:p-9">
                 <h3 className="mb-5 text-title-md2 font-bold text-black dark:text-white">
-                  {performance?.event}
+                  Today's Key Economic Event: {performance?.event}
                 </h3>
               </div>
-              <EconomicEventStat events={lastEconomicEvent} />
+              {
+                (shouldRenderPastSymbolPerformance(performance)) ? 
+                  <EconomicEventStat firstNumber={performance.performance.in_1_hour[performance.performance.in_1_hour.length - 1].toFixed(2) + "%"} secondNumber={performance.performance.in_1_day[performance.performance.in_1_day.length - 1].toFixed(2) + "%"} thirdNumber={(performance.performance.in_1_hour.reduce((a, b) => a + b, 0) / performance.performance.in_1_hour.length).toFixed(2) + "%"} fourthNumber={(performance.performance.in_1_day.reduce((a, b) => a + b, 0) / performance.performance.in_1_day.length).toFixed(2) + "%"} firstText={"Last 1 hour SPY change"} secondText={"Last 1 day SPY change"} thirdText={"Average 1 hour SPY change"} fourthText={"Average 1 day SPY change"} />
+                :
+                  <EconomicEventStat firstNumber={lastEconomicEvent?.actual.toFixed(2)} secondNumber={lastEconomicEvent?.estimate !== null ? lastEconomicEvent.estimate.toFixed(2) : "N/A"} thirdNumber={lastEconomicEvent?.previous.toFixed(2)} fourthNumber={lastEconomicEvent?.change_percentage.toFixed(2)} firstText={"Actual"} secondText={"Estimate"} thirdText={"Previous"} fourthText={"Diff from expected"} />
+              }
             </div>
+            
               <SinpleLineChart {...createHistoricalPerformance(performance)} />
           </>
+    
         );
       };
 
