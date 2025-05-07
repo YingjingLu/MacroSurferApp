8f6339b188ab5d76722e1a0227113299921d1332
YingjingLu <yingjinglu2019us@gmail.com>
2025-05-06 22:13:10 -0700

    fix endpoint

--- src/gobal/constants.ts
+++ src/gobal/constants.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:5080";
-// export const BACKEND_URL = "https://macrosurfer-backend-1040770150090.us-central1.run.app";
+// export const BACKEND_URL = "http://localhost:5080";
+export const BACKEND_URL = "https://macrosurfer-backend-1040770150090.us-central1.run.app";
