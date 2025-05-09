37a574ec966694bb666154c72cbba838d46c030a
YingjingLu <yingjinglu2019us@gmail.com>
2025-05-09 08:40:56 -0700

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,13 @@ import 'jsvectormap/dist/css/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
 import 'nouislider/dist/nouislider.css';
 import 'dropzone/dist/dropzone.css';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
       <App />
+      <Analytics />
     </Router>
   </React.StrictMode>
 );
